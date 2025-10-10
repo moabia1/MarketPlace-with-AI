fc1eb75cac253edb2cc2046b4e3ea8878db74e5b
moabia1 <mdmoabia81@gmail.com>
2025-10-10 23:29:20 +0530

    product service set-up

---
+++ product/src/db/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+async function connectDB() {
+  try {
+    
+  } catch (error) {
+    console.log("Database connection error ",error)
+  }
+}
+
+module.exports = connectDB
