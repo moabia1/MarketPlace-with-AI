ca2254a682ab6ef95a76225d527138182f209772
moabia1 <mdmoabia81@gmail.com>
2025-10-10 23:47:54 +0530

    set-up devdependencies

--- product/src/db/db.js
+++ product/src/db/db.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
   try {
-    
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log("MongoDb Connected")
   } catch (error) {
     console.log("Database connection error ",error)
   }
